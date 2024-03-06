4f76aa60c59dc799104493d3a56fb5adaa8a31ee
Jeff Chebul <jmchebul@jmchebul.com>
2024-03-06 09:22:16 -0600

    minor changes

--- geom.py
+++
@@ -1,8 +0,0 @@
-from math import pi, sqrt
-print('The geometry.py file has been executed')
-
-def area_of_circle(radius):
-    return pi * (radius**2)
-
-def hypot(b, c):
-    return sqrt((b**2)+(c**2))
